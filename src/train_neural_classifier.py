bab132b062aca9d1ed13fe430c07f36091762477
Alex Chandler <alex3@Alexs-MacBook-Pro-4.local>
2022-11-16 16:56:30 -0600

    fixed genre issue

--- src/train_neural_classifier.py
+++ src/train_neural_classifier.py
@@ -104,11 +104,11 @@ if __name__ == "__main__":
     args.num_epochs = 5
     args.model_name = "M5"
     args.audio_folder_path = "data/fma_small"
-    args.sampling_freq = 500_000 
+    args.sampling_freq = 8_000 
     args.padding_length = None
-    args.truncation_length = None #1300000
+    args.truncation_length = 200_000 #1300000
     args.convert_one_channel = True
-    # args.load_dataset_path = "logs/datasets/dataset_fma_small_one_channel"
+    # args.load_dataset_path = "logs/datasets/dataset_fma_small_one_channel_torch_4k_samples500_000"
     args.load_dataset_path = None
     args.debug = True  # TODO delete
     args.desired_dataset_name = "dataset_fma_small_one_channel_torch_4k_samples500_000"
@@ -124,13 +124,12 @@ if __name__ == "__main__":
         "truncation_len" : args.truncation_length,
         "convert_one_channel": args.convert_one_channel
     }
-    num_genres = 8
-    if args.model_name == "M5":
-        n_input = 1_300_000 # TODO: likely need to change
-        if args.convert_one_channel:
+    if args.convert_one_channel:
             in_channels = 1
-        else:
-            in_channels = 2
+    else:
+        in_channels = 2
+    if args.model_name == "M5":
+        # n_input = 1_300_000 # TODO: likely need to change
         model = M5(n_input=in_channels, n_output=num_genres) 
         lr = 1e-4
         # can also experiment with different parameters
@@ -142,9 +141,8 @@ if __name__ == "__main__":
         test_description = "Testing M5 CNN model on test data"
     
     elif args.model_name == "FF":
-        n_input = 1_300_000
-        num_genres = 8
-        model = FF(n_input=n_input, n_output=num_genres)
+        # n_input = 1_300_000
+        model = FF(n_input=in_channels, n_output=num_genres)
         optimizer = optim.Adam(model.parameters(), lr=1e-3)
         epochs = 50
         criterion = nn.SGD()
@@ -157,7 +155,6 @@ if __name__ == "__main__":
             dataset = pickle.load(input_file)
     else:
         dataset = AudioDataset(meta_data_path = "data/fma_metadata", audio_folder_path = args.audio_folder_path, preprocessing_dict = preprocessing_dict, debug = args.debug, datatype = args.datatype)
-        print("poop")
         # save dataset in logs/datasets
         with open("logs/datasets/"+args.desired_dataset_name, "wb") as output_file:
             pickle.dump(dataset, output_file)
--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -5,41 +5,66 @@ import numpy as np
 # import matplotlib.pyplot as plt
 from torch.utils.data import Dataset
 from utils.dataprocessing import * 
-
+import ast
+# import math
+import random
 # import h5py
 
 
 class AudioDataset(Dataset):
 
     def __init__(self, meta_data_path, audio_folder_path, preprocessing_dict = {}, debug = False, datatype = "torch"):
+        # genres_jupyter_csv = utils.load('data/fma_metadata/genres.csv')
+        tracks_jupyter_csv = load('data/fma_metadata/tracks.csv')
+        genres_jupyter = tracks_jupyter_csv.loc[:, ('track', 'genre_top')]
+
         track_csv = pd.read_csv(meta_data_path+"/tracks.csv", header=1).iloc[1: , :]
         genres_csv = track_csv["genre_top"]
         audio_tensors = [] 
         genres = [] 
-
+        # invalid_genres = set(['nan'])
+        accepted_genres = set(['Experimental', 'Pop', 'Folk', 'Electronic',
+       'Rock', 'Hip-Hop', 'Instrumental', 'Jazz']) # took out 'International' and "nan"
         not_found_cnt = 0
         torch_audio_read_error_cnt = 0
         small_audio_file_cnt = 0
         for subdir, dirs, files in os.walk(audio_folder_path):
             for filename in files:
-                print(len(genres))
+                # print("Start loop: {}".format(len(genres))) 
                 if filename.endswith(('.mp3')):
-                    if debug and len(audio_tensors) > 4000:
+                    if debug and len(audio_tensors) > 1000:
                         break
                     track_id = eval(filename.rstrip(".mp3").lstrip('0')) 
-                    track_csv_index = track_csv.index[track_csv["Unnamed: 0"] == track_id].tolist()
-                    if not track_csv_index:
+                    # track_csv_index = track_csv.index[track_csv["Unnamed: 0"] == track_id].tolist()
+                    # if not track_csv_index:
+                    #     not_found_cnt +=1
+                    #     continue
+                    # assert len(track_csv_index) == 1
+                    # genre = genres_csv.iloc[track_csv_index[0]]
+                    # verify genre
+                    try:
+                        genre = genres_jupyter[track_id]
+                    except KeyError:
                         not_found_cnt +=1
                         continue
-                    # assert len(track_csv_index) == 1
-                    genre = genres_csv.iloc[track_csv_index[0]]
+                    if genre not in accepted_genres:
+                        continue
+                    # if genre != genre_query_method_2:
+                    #     print("manual check")
+                    # assert genre == genre_query_method_2
+                    # if genre in invalid_genres:
+                    #     continue
+                    # if math.isnan(genre):
+                        # continue
                     #print os.path.join(subdir, file)
                     filepath = subdir + os.sep + filename
+                    # print("Loading audio: {}".format(len(genres))) 
                     try:
                         data_waveform, rate_of_sample = torchaudio.load(filepath)
                         preprocessing_dict["orig_freq"] = rate_of_sample
                     except Exception as e:
                         torch_audio_read_error_cnt +=1
+                    # print("Applying processing: {}".format(len(genres))) 
                     data_waveform = self.apply_preproccess(data_waveform, preprocessing_dict)
                     if datatype == "np":
                         data_waveform = data_waveform.detach().numpy()
@@ -53,8 +78,12 @@ class AudioDataset(Dataset):
         audio_tensors= np.concatenate(audio_tensors)
         genres= np.array(genres)
 
+        
+        # genres = genres
+        temp = list(zip(audio_tensors, genres))
+        random.shuffle(temp)
+        audio_tensors, genres = zip(*temp)
         self.audio_tensors = audio_tensors
-        genres = genres
         self.genres_factorized = pd.factorize(pd.Series(genres))
 
 
@@ -76,6 +105,7 @@ class AudioDataset(Dataset):
         if sampling_freq != None:
             # orig_freq, new_freq = sampling["orig_freq"], sampling["new_freq"]
             waveform = resample(waveform, orig_freq, sampling_freq)
+            print(waveform.shape)
 
         # Not necessary for our project
         if padding_length != None:
@@ -93,3 +123,50 @@ class AudioDataset(Dataset):
 
     def __getitem__(self, idx):        
         return self.audio_tensors[idx], self.genres_factorized[0][idx]
+
+
+# directly copied from original data location: This is a helper file for loading in the dataset https://github.com/mdeff/fma/blob/master/utils.py
+def load(filepath):
+
+    filename = os.path.basename(filepath)
+
+    if 'features' in filename:
+        return pd.read_csv(filepath, index_col=0, header=[0, 1, 2])
+
+    if 'echonest' in filename:
+        return pd.read_csv(filepath, index_col=0, header=[0, 1, 2])
+
+    if 'genres' in filename:
+        return pd.read_csv(filepath, index_col=0)
+
+    if 'tracks' in filename:
+        tracks = pd.read_csv(filepath, index_col=0, header=[0, 1])
+
+        COLUMNS = [('track', 'tags'), ('album', 'tags'), ('artist', 'tags'),
+                   ('track', 'genres'), ('track', 'genres_all')]
+        for column in COLUMNS:
+            tracks[column] = tracks[column].map(ast.literal_eval)
+
+        COLUMNS = [('track', 'date_created'), ('track', 'date_recorded'),
+                   ('album', 'date_created'), ('album', 'date_released'),
+                   ('artist', 'date_created'), ('artist', 'active_year_begin'),
+                   ('artist', 'active_year_end')]
+        for column in COLUMNS:
+            tracks[column] = pd.to_datetime(tracks[column])
+
+        SUBSETS = ('small', 'medium', 'large')
+        try:
+            tracks['set', 'subset'] = tracks['set', 'subset'].astype(
+                    'category', categories=SUBSETS, ordered=True)
+        except (ValueError, TypeError):
+            # the categories and ordered arguments were removed in pandas 0.25
+            tracks['set', 'subset'] = tracks['set', 'subset'].astype(
+                     pd.CategoricalDtype(categories=SUBSETS, ordered=True))
+
+        COLUMNS = [('track', 'genre_top'), ('track', 'license'),
+                   ('album', 'type'), ('album', 'information'),
+                   ('artist', 'bio')]
+        for column in COLUMNS:
+            tracks[column] = tracks[column].astype('category')
+
+        return tracks
